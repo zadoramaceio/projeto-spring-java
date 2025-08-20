7fbff12e4ea746c1a50fde863c9a3eaca0461238
zadoramaceio <dora13.isa@gmail.com>
2025-08-20 19:47:58 -0300

    primeira aula finalizada

---
+++ src/main/java/com/pimentadakota/dslist/services/GameService.java
@@ -0,0 +1,23 @@
+package com.pimentadakota.dslist.services;
+
+import com.pimentadakota.dslist.dto.GameMinDTO;
+import com.pimentadakota.dslist.entities.Game;
+import com.pimentadakota.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll(){
+        var result = gameRepository.findAll();
+        return  result.stream().map(x -> new GameMinDTO(x)).toList();
+
+
+
+    }
+}
