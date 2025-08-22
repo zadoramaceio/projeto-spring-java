02b440b92301f5b9398de3ac561c3f935c8587c8
zadoramaceio <dora13.isa@gmail.com>
2025-08-21 21:27:18 -0300

    homolog postgres ok

--- src/main/java/com/pimentadakota/dslist/projections/GameMinProjection.java
+++ src/main/java/com/pimentadakota/dslist/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.pimentadakota.dslist.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
